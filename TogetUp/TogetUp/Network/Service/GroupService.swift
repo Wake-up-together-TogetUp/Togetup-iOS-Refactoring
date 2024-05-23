10cfd393e074e07d88fabd09334815d23cdd7970
angryeon7 <nayeon7612@gmail.com>
2024-05-23 15:15:25 +0900

    Feat: GroupService 수정 및 구현

--- TogetUp/TogetUp/Network/Service/GroupService.swift
+++ TogetUp/TogetUp/Network/Service/GroupService.swift
@@ -2,77 +2,26 @@
 //  GroupService.swift
 //  TogetUp
 //
-//  Created by 이예원 on 2023/10/12.
+//  Created by nayeon  on 4/20/24.
 //
 
 import Foundation
 import Moya
+import RxSwift
 
-enum GroupService {
-    case getGroupList
-    case createGroup(param: CreateGroupRequest)
-    case getMissionLog(roomId: Int, localDateTime: String)
-    case getGroupDetail(roomId: Int)
-    case deleteMember(roomId: Int)
-    case getGroupDetailWithCode(code: String)
+class GroupService {
+    
+    private let provider: MoyaProvider<GroupAPI>
+    private let networkManager = NetworkManager()
+    
+    init(provider: MoyaProvider<GroupAPI> = MoyaProvider<GroupAPI>()) {
+        self.provider = provider
+    }
+    
+    // 그룹 리스트 조회
+    func getGroupList() -> Single<Result<GetGroupListResponse, NetWorkingError>> {
+        let request = provider.rx.request(.getGroupList)
+        return networkManager.handleAPIRequest(request, dataType: GetGroupListResponse.self)
+    }
 }
 
-extension GroupService: TargetType {
-    var baseURL: URL {
-        return URL(string: URLConstant.baseURL)!
-    }
-    
-    var path: String {
-        switch self {
-        case .getGroupList:
-            return URLConstant.getGroupList
-        case .createGroup:
-            return URLConstant.createGroup
-        case .getGroupDetail(let roomId):
-            return URLConstant.getGroupDetail + "\(roomId)"
-        case .getMissionLog:
-            return URLConstant.getMissionLog
-        case .deleteMember(let roomId):
-            return URLConstant.deleteMember + "\(roomId)" + "/member"
-        case .getGroupDetailWithCode:
-            return URLConstant.getGroupDetailWithCode
-        }
-    }
-    
-    var method: Moya.Method {
-        switch self {
-        case .getGroupList, .getGroupDetail,.getMissionLog, .getGroupDetailWithCode:
-            return .get
-        case .createGroup:
-            return .post
-        case .deleteMember:
-            return .delete
-        }
-    }
-    
-    var task: Moya.Task {
-        switch self {
-        case .getGroupList, .getGroupDetail, .deleteMember:
-            return .requestPlain
-        case .createGroup(let param):
-            return .requestJSONEncodable(param)
-        case .getMissionLog(let roomId, let date):
-            let fixedTime = "11:55:38"
-            let fullDateTime = "\(date) \(fixedTime)"
-            return .requestParameters(parameters: ["roomId": roomId, "localDateTime": fullDateTime], encoding: URLEncoding.default)
-        case .getGroupDetailWithCode(let code):
-            return .requestParameters(parameters: ["invitationCode" : code], encoding: URLEncoding.default)
-        }
-    }
-    
-    var headers: [String : String]? {
-        switch self {
-        case .getGroupList, .createGroup, .getGroupDetail, .getMissionLog, .deleteMember, .getGroupDetailWithCode:
-            let token = KeyChainManager.shared.getToken()
-            return [
-                "Authorization": "Bearer \(token ?? "")",
-                "Content-Type": "application/json"
-            ]
-        }
-    }
-}
