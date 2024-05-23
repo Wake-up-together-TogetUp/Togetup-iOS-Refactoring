e630377e6e70ae30cd548b2531c6cd1c55619e69
angryeon7 <nayeon7612@gmail.com>
2024-05-23 15:19:13 +0900

    Feat: GroupViewModel 생성

---
+++ TogetUp/TogetUp/Screen/Group/ViewModel/GroupListViewModel.swift
@@ -0,0 +1,70 @@
+//
+//  GroupListViewModel.swift
+//  TogetUp
+//
+//  Created by nayeon  on 5/20/24.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class GroupViewModel: ViewModelType {
+
+    struct Input {
+        let fetchGroupList: PublishSubject<Void>
+    }
+
+    struct Output {
+        let groupList: Driver<[GroupResult]>
+        let error: Driver<String>
+    }
+
+    var disposeBag = DisposeBag()
+
+    private let groupService: GroupService
+
+    init(groupService: GroupService) {
+        self.groupService = groupService
+    }
+
+    func transform(input: Input) -> Output {
+        let errorSubject = PublishSubject<String>()
+
+        let groupList = input.fetchGroupList
+            .flatMapLatest { [unowned self] in
+                self.groupService.getGroupList()
+                    .asObservable()
+                    .materialize()
+            }
+            .share()
+
+        let groupListResult = groupList
+            .compactMap { event -> [GroupResult]? in
+                switch event {
+                case .next(let result):
+                    switch result {
+                    case .success(let response):
+                        return response.result
+                    case .failure(let error):
+                        errorSubject.onNext(self.networkManager.errorMessage(for: error))
+                        return nil
+                    }
+                case .error(let error):
+                    errorSubject.onNext(self.networkManager.errorMessage(for: error))
+                    return nil
+                case .completed:
+                    return nil
+                }
+            }
+            .asDriver(onErrorJustReturn: [])
+
+        let error = errorSubject.asDriver(onErrorJustReturn: "알 수 없는 에러가 발생했습니다.")
+
+        return Output(groupList: groupListResult, error: error)
+    }
+
+    private var networkManager: NetworkManager {
+        return NetworkManager()
+    }
+}
