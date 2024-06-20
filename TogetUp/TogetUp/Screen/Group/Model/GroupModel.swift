e4129939b5c6b90275b3ef367aa2fea148884d5f
angryeon7 <nayeon7612@gmail.com>
2024-06-21 00:06:21 +0900

    Refactor: Group 관련 모델 수정

--- TogetUp/TogetUp/Screen/Group/Model/GroupModel.swift
+++ TogetUp/TogetUp/Screen/Group/Model/GroupModel.swift
@@ -5,6 +5,33 @@
 //  Created by nayeon  on 5/20/24.
 //
 
+struct CreateGroupResponse: Codable {
+    let httpStatusCode: Int
+    let httpReasonPhrase: String
+    let message: String
+}
+
+struct CreateGroupRequest: Codable {
+    let name: String
+    let intro: String
+    let alarmCreateReq: GroupAlarmRequest
+}
+
+struct GroupAlarmRequest: Codable {
+    let name: String
+    let alarmTime: String
+    let monday: Bool
+    let tuesday: Bool
+    let wednesday: Bool
+    let thursday: Bool
+    let friday: Bool
+    let saturday: Bool
+    let sunday: Bool
+    let isVibrate: Bool
+    let missionId: Int
+    let missionObjectId: Int?
+}
+
 struct GetGroupListResponse: Codable {
     let httpStatusCode: Int
     let httpReasonPhrase: String
